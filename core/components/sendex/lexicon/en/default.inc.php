38388bf89a3d011546fe8c149565bf63585af5cd
anatoliy <believe.koenig@gmail.com>
2016-05-07 19:44:42 +0200

    Newsletter table columns

--- core/components/sendex/lexicon/en/default.inc.php
+++ core/components/sendex/lexicon/en/default.inc.php
@@ -9,6 +9,7 @@ $_lang['sendex_newsletters'] = 'Newsletters';
 $_lang['sendex_newsletters_intro'] = 'On this page you create and edit your subscription.';
 
 $_lang['sendex_btn_create'] = 'Create';
+
 $_lang['sendex_newsletter_err_ae'] = 'An newsletter already exists with that name.';
 $_lang['sendex_newsletter_err_nf'] = 'Newsletter not found.';
 $_lang['sendex_newsletter_err_ns'] = 'Newsletter not specified.';
@@ -17,3 +18,15 @@ $_lang['sendex_newsletter_err_save'] = 'An error occurred while trying to save t
 $_lang['sendex_newsletter_remove'] = 'Remove newsletter';
 $_lang['sendex_newsletter_remove_confirm'] = 'Are you sure you want to remove this newsletter?';
 $_lang['sendex_newsletter_update'] = 'Update newsletter';
+
+$_lang['sendex_newsletter_id'] = 'id';
+$_lang['sendex_newsletter_name'] = 'Title';
+$_lang['sendex_newsletter_description'] = 'Description';
+$_lang['sendex_newsletter_active'] = 'Active';
+$_lang['sendex_newsletter_template'] = 'Template';
+$_lang['sendex_newsletter_snippet'] = 'Snippet';
+$_lang['sendex_newsletter_email_subject'] = 'Email subject';
+$_lang['sendex_newsletter_email_from'] = 'Emil from';
+$_lang['sendex_newsletter_email_from_name'] = 'Email from name';
+$_lang['sendex_newsletter_email_reply'] = 'Email reply';
+$_lang['sendex_newsletter_image'] = 'Image';
--- core/components/sendex/lexicon/ru/default.inc.php
+++ core/components/sendex/lexicon/ru/default.inc.php
@@ -9,6 +9,7 @@ $_lang['sendex_newsletters'] = 'Подписки';
 $_lang['sendex_newsletters_intro'] = 'На этой странице вы создаёте и редактируете ваши подписки.';
 
 $_lang['sendex_btn_create'] = 'Создать';
+
 $_lang['sendex_newsletter_err_ae'] = 'Подписка с таким именем уже существует.';
 $_lang['sendex_newsletter_err_nf'] = 'Подписка не найдена.';
 $_lang['sendex_newsletter_err_ns'] = 'Подписка не указан.';
@@ -17,3 +18,15 @@ $_lang['sendex_newsletter_err_save'] = 'Ошибка при сохранении
 $_lang['sendex_newsletter_remove'] = 'Удалить подписку';
 $_lang['sendex_newsletter_remove_confirm'] = 'Вы уверены, что хотите удалить эту подписку?';
 $_lang['sendex_newsletter_update'] = 'Изменить подписку';
+
+$_lang['sendex_newsletter_id'] = 'id';
+$_lang['sendex_newsletter_name'] = 'Название';
+$_lang['sendex_newsletter_description'] = 'Описание';
+$_lang['sendex_newsletter_active'] = 'Включено';
+$_lang['sendex_newsletter_template'] = 'Шаблон';
+$_lang['sendex_newsletter_snippet'] = 'Сниппет';
+$_lang['sendex_newsletter_email_subject'] = 'Тема письма';
+$_lang['sendex_newsletter_email_from'] = 'Исходящий email';
+$_lang['sendex_newsletter_email_from_name'] = 'Отправитель';
+$_lang['sendex_newsletter_email_reply'] = 'Ответный email';
+$_lang['sendex_newsletter_image'] = 'Изображение';
